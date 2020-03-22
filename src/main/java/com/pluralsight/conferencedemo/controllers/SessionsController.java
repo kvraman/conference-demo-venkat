860db9a5009f70c2c9517135988e4586839feb02
Venkat Kalyan <Venkat_Kalyan@hotmail.com>
2020-03-22 14:59:44 +0530

    Initial version of conference demo

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,49 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.hibernate.annotations.Cascade;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        List<Session> sessions = sessionRepository.findAll();
+        return sessions;
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id})", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // TODO: Research how we can delete children
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody final Session session) {
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+
+}
